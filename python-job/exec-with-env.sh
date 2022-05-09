6d4379214784106096e6c1b5b82e1502c3a42265
Felix Heilmeyer <code@fehe.eu>
2022-05-09 21:49:12 +0200

    feat(python-job): run without init system
    the workflow emissary executor will act as PID 1

--- python-job/exec-with-env.sh
+++ python-job/exec-with-env.sh
@@ -1,7 +1,34 @@
-#!/usr/bin/with-contenv bash
+#!/bin/bash
+
+echo "Populating home directory"
+cp -r -n /tmp_home/* /home/
+
+echo "Initializing conda"
 . /opt/conda/etc/profile.d/conda.sh
 
 set -e
+
+conda activate base
+
+WORK_PATH="${WORKDIR:-/work}"
+
+export CONDA_ENV_PATH="${WORK_PATH}/${CONDA_FILE:-environment.yml}"
+export PIP_REQUIREMENTS_PATH="${WORK_PATH}/${PIP_REQUIREMENTS:-requirements.txt}"
+
+if [ -f "${CONDA_ENV_PATH}" ]; then
+  mamba env create -f "${CONDA_ENV_PATH}"
+  export ENV_NAME=`cat "${CONDA_ENV_PATH}" | grep 'name: ' | sed 's/name: \(.*\)/\1/'`
+  echo "${ENV_NAME}" >> /tmp/CONDA_ENV
+  conda activate "${ENV_NAME}"
+else
+  echo "base" >> /tmp/CONDA_ENV
+fi
+
+if [ -f "${PIP_REQUIREMENTS_PATH}" ]; then
+  pip install -r "${PIP_REQUIREMENTS_PATH}"
+fi
+
+python -m ipykernel install --user --name 'job-kernel' || true
 
 export CONDA_ENV=`cat /tmp/CONDA_ENV`
 
@@ -14,3 +41,9 @@ echo "-----------------"
 conda activate "${CONDA_ENV}"
 
 exec "$@"
+
+echo "-----------------"
+echo "Job finished"
+echo "-----------------"
+
+exit 0

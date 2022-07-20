4728747432193ed15630763cd5fe5ef7dad43419
Felix Heilmeyer <code@fehe.eu>
2022-07-20 20:12:50 +0200

    fix screensaver script and more verbose output

--- matlab-interactive/entrypoint.sh
+++ matlab-interactive/entrypoint.sh
@@ -4,7 +4,11 @@ cp -r /tmp_home/. /home/jovyan
 mkdir -p /opt/noVNC`dirname ${NB_PREFIX}`
 ln -s /opt/noVNC /opt/noVNC${NB_PREFIX}
 
+cat > ~/.config/xfce4/xinitrc <<EOF
+#!/bin/sh
 xset s off
 xset s noblank
+. /etc/xdg/xfce4/xinitrc
+EOF
 
 exec /bin/run.sh "$@"

61e76f4015878517876dba0f7c78ee2cc773530d
Felix Heilmeyer <code@fehe.eu>
2022-07-20 19:26:52 +0200

    fix: correct redirect and disable screensaver

--- matlab-interactive/entrypoint.sh
+++ matlab-interactive/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cp -r /tmp_home/. /home/jovyan
+
+mkdir -p /opt/noVNC`dirname ${NB_PREFIX}`
+ln -s /opt/noVNC /opt/noVNC${NB_PREFIX}
+
+xset s off
+xset s noblank
+
 exec /bin/run.sh "$@"
